b2ab86045581784ddb8c002b19a9ef373b732017
Natalie <nathompson110@gmail.com>
2017-10-13 15:30:11 -0500

    geocode and directions functions completed

--- task-manager/client/src/utils/helpers.js
+++ task-manager/client/src/utils/helpers.js
@@ -7,7 +7,7 @@
 const googleMapsClient = require('@google/maps').createClient({
   key: "AIzaSyCicRPM6zzE6VFEw7iN4ScfF_VVSppnBlQ"
 });
-
+//This should run anytime an actual address location is listed during creation.
 const helpers = {
 geocoder: function(address, cb) {
 let res = [];
@@ -24,7 +24,7 @@ googleMapsClient.geocode({
 
 
   }
-  cb(res);
+  // cb(res);
 })
 
 
@@ -44,49 +44,57 @@ distances: function(latlon, destLatandLon){
 
 
 // TURN BY TURN directions
-    directions: function(latlon, destLatandLon){
-        return googleMapsClient.directions({
-         origin: latlon,
-         destination: destLatandLon
+    directions: function(resCoordArray){
+      let resCoord = resCoordArray 
+        googleMapsClient.directions({
+         origin: [resCoordArray[0], resCoordArray[1]],
+         destination: [resCoordArray[2], resCoordArray[3]]
     }, function(err, response) {
   if (!err) {
+    // response.json.routes[0].legs[0].steps.forEach(function(element) {
+    //    console.log(element.html_instructions) 
+    // } );
+    // console.log(response.json.routes[0].legs[0].distance.text);
+    // console.log(response.json.routes[0].legs[0].duration.text)
+    resCoord.push(response.json.routes[0].legs[0].distance.text);
+    resCoord.push(response.json.routes[0].legs[0].duration.text);
     response.json.routes[0].legs[0].steps.forEach(function(element) {
-       console.log(element.html_instructions) 
-    } );
-    console.log(response.json.routes[0].legs[0].distance.text);
-    console.log(response.json.routes[0].legs[0].duration.text)
+       resCoord.push(element.html_instructions)});
+       console.log(resCoord)
 
 
   }
     })
 },
-places: function(originCoord){
+places: function(originCoordArray, taskPlace, callback){
         //let originCoord = helpers.geocoder(address);
         let resCoord = []
         googleMapsClient.places({
-            query: 'Target',
+            query: taskPlace,
            
-            location: originCoord,
-            radius: 1609
+            location: originCoordArray,
+            radius: 3209
 
         }, function(err, response) {
         if (response) {
-                resCoord.push(originCoord[0])
-                resCoord.push(originCoord[1])
+                resCoord.push(originCoordArray[0])
+                resCoord.push(originCoordArray[1])
+                resCoord.push(taskPlace)
                 resCoord.push(response.json.results[0].geometry.location.lat)
                 resCoord.push(response.json.results[0].geometry.location.lng);
                 resCoord.push(response.json.results[0].formatted_address);
                
     // console.log(response.json.results[0].geometry.location);
-    console.log(resCoord)
+    // console.log(resCoord)
     //resCoord[0], resCoord[1]=original point's coord
      //resCoord[2], resCoord[3]=place's point's coord
       //resCoord[4]= address of place
       
         
-        return resCoord
+        // return resCoord
 
     }
+    if(callback) callback(resCoord)
  
     });
 
@@ -96,8 +104,8 @@ places: function(originCoord){
 // export default helpers;
 
 //Example of working places function being called
-helpers.geocoder( "4016 N Central Park Ave, Chicago, IL 60618", helpers.places);
-//Example of working directions being called
+// helpers.geocoder( "4016 N Central Park Ave, Chicago, IL 60618");
+//Example of working directions bAeing called
 // helpers.directions([41.9456545, -87.702458 ], [ 41.9544384, -87.7178357 ]);
-
+helpers.places([ 41.9544384, -87.7178357 ], "Jewel", helpers.directions);
 
