d78dc5d34999a90e40f25e4c32b0bd5b087673af
Seana Curran <shaunacurran@Shaunas-MacBook-Pro.local>
2017-10-13 20:56:16 -0500

    'landing page'

--- task-manager/client/src/App.js
+++ task-manager/client/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import image from './images/biker.png'
 // css file temp commented out
 import styles from './uikit.css';
+import navbar from './navbar.css';
 
 import GoogleApiWrapper from './components/Map.js';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
@@ -14,55 +15,37 @@ import AddTask from './components/AddTask.js';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
+      // <div className="App">
+      //   <div className="App-header">
         
-          <h2>Task Force</h2>
-        </div>
-        
+      //     <h2>Task Force</h2>
+      //   </div>
+      <div>
         <Router>
-        <nav className="uk-position-relative" uk-navbar>
-          <img src={image} alt="image of cyclist"/>
-          <div className="uk-position-top">
-              <ul className="uk-navbar-nav">
-              <div className="uk-navbar-left">
-                  <li className="uk-active"><a href="#">Active</a></li>
-                  <li>
-                      <a href="#">Parent</a>
-                      <div className="uk-navbar-dropdown">
-                          <ul className="uk-nav uk-navbar-dropdown-nav">
-                              <li className="uk-active"><a href="#">Active</a></li>
-                              <li><a href="#">Item</a></li>
-                              <li><a href="#">Item</a></li>
-                          </ul>
-                      </div>
-                  </li>
-                  <li><a href="#">Item</a></li>
-                  </div>
-              </ul>
-          </div>
-        </nav>
-        {/* <div className="uk-position-relative">
+          <nav className="uk-navbar-container uk-margin" uk-navbar="mode: click">
+            <img src={image} alt="image of cyclist"/>
             <div className="uk-position-top">
-              <nav className="uk-navbar-container uk-navbar-transparent uk-navbar">
-                <div className="uk-navbar-left">
-                  <Link to="/map">See Map</Link> |
-                  <Link to="/addTask">Add A Task</Link> |
-                  <Link to="/toDoList">To-Do List</Link> |
+              <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar>
+                <div className="uk-navbar-right">
+                  <ul className="uk-navbar-nav">
+                    <li className="uk-active"><Link to="/map/">Map</Link></li>
+                    <li className="uk-active"><Link to = "/addtask/">Tasks</Link></li>
+                    <li className="uk-active"><Link to = "/toDoList/">To-Do List</Link></li>
+                  </ul>
                 </div>
               </nav>
             </div>
-            <Route exact path ="/map" component={ GoogleApiWrapper }/>
-            <Route exact path ="/addTask" component={ AddTask }/>
-            <Route exact path ="/toDoList" component={ AddStudent }/>
-        </div> */}
+              <Route exact path ="/map" component={ GoogleApiWrapper }/>
+              <Route exact path ="/addTask" component={ AddTask }/>
+              <Route exact path ="/toDoList" component={ AddStudent }/>
+        </nav>
+        </Router>
+        </div>
+       
          
-       </Router>  
          
-        <p className="App-intro">
-          
-        </p>
-      </div>
+    
+      
     );
   }
 }
