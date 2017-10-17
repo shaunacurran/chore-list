ee8b270c19768828fd7a9e57fe44ceb5be900f66
Karen <K.Blanchette07@gmail.com>
2017-10-16 20:08:57 -0500

    task stuff

--- task-manager/models/task.js
+++ task-manager/models/task.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const taskSchema = new Schema({
-    createdDate: {type: String, required: true},
     createdBy: {type: String, required: true},
     taskName:{type: String, requied: true},
-    taskDate: {},
-    taskTime: {},
-    taskLocation: {}
+    taskStatus:{type: String, required: true},
+    taskType: {type: String, requied:true},
+    taskDate: {type: Date, required: true},
+    taskTime: {type: Time, required: true},
+    taskLocation: {type: String, required}
 });
 
 const Task = mongoose.model("Task", taskSchema);
