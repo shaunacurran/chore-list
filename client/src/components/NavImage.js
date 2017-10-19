209d489820cec2257c4cd205931b37914a3e95f1
Shauna Curran <shaunaccurran@gmail.com>
2017-10-18 19:55:05 -0500

    'navbar'

--- client/src/components/NavImage.js
+++ client/src/components/NavImage.js
@@ -11,7 +11,7 @@ class NavImage extends Component {
 
 render() {
     return (
-    <div>
+    <div className = "main">
       
     <div className = "bikeContainer">
       <img className = "cyclist" src={image} alt="image of cyclist"/>
